7c7a7ac864044b13a4eb4b2e363c9f15812925a6
Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-03-17 13:37:01 +0000

    Deploying to gh-pages from @ Murlykin/react-practice-routing@7d13439ad98e387fc0a8242a11f894e4b232a575 🚀

---
+++ static/js/493.496287e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_practice_routing=self.webpackChunkreact_practice_routing||[]).push([[493],{493:function(i,e,t){t.r(e);var a=t(184);e.default=function(){return(0,a.jsxs)("main",{children:[(0,a.jsx)("h1",{children:"Welcome"}),(0,a.jsx)("img",{src:"https://via.placeholder.com/960x240",alt:""}),(0,a.jsx)("p",{children:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, laboriosam placeat incidunt rem illum animi nemo quibusdam quia voluptatum voluptate."})]})}}}]);
+//# sourceMappingURL=493.496287e7.chunk.js.map
